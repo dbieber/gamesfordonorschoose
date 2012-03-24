cac338fc04827b1cdbdcd11389eb3671b389a079
Bonnie <bonnie.eisenman@gmail.com>
2012-03-24 19:51:25 -0400

    added three projects

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-import featuredProj
+import threeProjs
 import kitty
 from flask import Flask
 from flask import render_template
@@ -12,9 +12,24 @@ def googleVerificationPage():
 
 @app.route("/")
 def hello():
-    url = featuredProj.parse(id)
+    l = threeProjs.parse()
+    img0 = l[0]['imageURL']
+    img1 = l[1]['imageURL']
+    img2 = l[2]['imageURL']
+    url0 = l[0]['proposalURL']
+    url1 = l[1]['proposalURL']
+    url2 = l[2]['proposalURL']
+    title0 = l[0]['title']
+    title1 = l[1]['title']
+    title2 = l[2]['title']
+
     kittyurl =  kitty.getRandom()
-    return render_template('index.html', title="Kitties for Kiddies!", imageURL=url, kittyURL = kittyurl)
+    return render_template('index.html', title="Kitties for Kiddies!", img0=img0, img1=img1, img2=img2,
+                           url0=url0, url1=url1, url2=url2,
+                           title0=title0, title1=title1, title2=title2,
+                           desc0 = l[0]['shortDescription'], desc1=l[1]['shortDescription'],
+                           desc2 = l[2]['shortDescription'],
+                           kittyURL = kittyurl)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
